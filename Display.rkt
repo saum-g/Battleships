9a18b7a0fa75f13b90bfe993fc9bf489ed378b78
Saumya Goyal <saumyagoyal@Saumyas-MacBook-Pro.local>
2019-03-30 10:45:49 +0530

    First Display

--- Display.rkt
+++ Display.rkt
@@ -1,1 +1,37 @@
 #lang racket
+(require "state-class.rkt" 2htdp/universe 2htdp/image lang/posn)
+
+;change the screen size variables so that display occupies full screen
+(provide screen-resize)
+(define (screen-resize state x y)
+  (send state set-screen-size! x y)
+  "Resized")
+
+;parameter of to-draw, draws the screen depending on state
+(define (screen state)
+  (define w (car (send state get-screen-size)))
+  (define h (cdr (send state get-screen-size)))
+  (define bckg (rectangle w h 'solid "white"))
+  (define pl1-grid (square (* 0.3 w) 'solid "lightblue"))
+  (define pl2-grid (square (* 0.3 w) 'solid "lightblue"))
+  ;(place-image (square (* 0.3 w) 'solid "lightblue") (* 0.25 w) (* 0.5 h) bckg)
+  ;(place-image (square (* 0.3 w) 'solid "lightblue") (* 0.75 w) (* 0.5 h) bckg))
+  (place-images (list pl1-grid pl2-grid) (list (make-posn (* 0.25 w) (* 0.5 h)) (make-posn (* 0.75 w) (* 0.5 h))) bckg))
+
+
+;initial state
+(define init-state
+  (new state%
+       [md 'placement]
+       [plyr 1]
+       [pl1 (new player%)]
+       [pl2 (new player%)]))
+(define (stop-fn state)
+  (pair? (send state get-screen-size)))
+
+(big-bang init-state
+  ;(display-mode 'fullscreen screen-resize)
+  (to-draw screen)
+  
+  (stop-when stop-fn screen)
+  )
--- state-class.rkt
+++ state-class.rkt
@@ -7,7 +7,8 @@
 (define (grid-ref grid r c)
   (vector-ref (vector-ref grid r) c))
 
- (define state%
+(provide state%)
+(define state%
   (class object%
     (super-new)
 
@@ -17,22 +18,33 @@
     (init pl1) (define player1 pl1)  ; these are instances (objects) of the class player%
     (init pl2) (define player2 pl2)
 
+    ;storing the size of screen, assuming 100X50 initially, changed during execution
+    (define screen-width 1000)
+    (define screen-height 500)
+    (define/public (set-screen-size! x y)
+      (set! screen-width x)
+      (set! screen-height y)
+      (cons x y))
+    (define/public (get-screen-size)
+      (cons screen-width screen-height))
+
     ;member functions
     
     
     ))
 
+(provide player%)
 (define player%
   (class object%
     (super-new)
     (define ships-vector
-      (build-vector 5 (lambda (x) (cons (string-append "ship" (~a (+ x 1))
-                                                       (make-vector 5 (cons -1 -1)))))))
+      (build-vector 5 (lambda (x) (cons (string-append "ship" (~a (+ x 1)))
+                                                       (make-vector 5 (cons -1 -1))))))
     ; vector containing 5 ships and their coordinates packed in a vector #((ship1 . #((1,2)(1,3))), (ship2 . #((4,5)(5,5)(6,5)) ...)
     (define ships-grid (build-grid 10 10 0))  ; grid pe kaha kaha ship hai
     (define strikes-grid (build-grid 10 10 0))  ; where all he has hit so far on the opponent's grid
     ; 0 -> no attempt  1 -> miss  2 -> hit  3-> ship sunk
-))
+    ))
     ;member functions to change these private fields (read data members)
 
     ;(define (fill-ships 
