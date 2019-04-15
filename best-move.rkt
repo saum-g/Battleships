7449dec91b65813c6489c4c65314aa05748612b5
shalabh147 <shalabh147@gmail.com>
2019-04-15 22:34:29 +0530

    Add function for random filling and determining grid for probability

--- best-move.rkt
+++ best-move.rkt
@@ -1,38 +1,110 @@
 #lang racket
-
 (require "state-class.rkt")
 (define (set-grid! grid r c v)
   (vector-set! (vector-ref grid r) c v))
 
-;returns the possible ways a part of a ship could be at x1,y1, provided the list of forbidden points and the length of the ship
-(define (ways-for-sqr x1 y1 forbid-pt-list ship-length)
-  (define highest-x 10)
-  (define highest-y 10)
-  (define least-x -1)
-  (define least-y -1)
-  ;determine highest and lowest x and y value in the same row/column which is forbidden
-  (map (lambda (coord) (cond [(= (car coord) x1) (cond [(< (cdr coord) highest-y) (set! highest-y (cdr coord))]
-                                                       [(> (cdr coord) least-y) (set! least-y (cdr coord))])]
-                             [(= (cdr coord) y1) (cond [(< (car coord) highest-x) (set! highest-x (car coord))]
-                                                       [(> (car coord) least-x) (set! least-x (car coord))])]))
-       forbid-pt-list)
-  ;calculate the number of blocks where a part of ship could be placed in each direction, keeping in mind that x1,y1 should always be occupied
-  (define free-blocks-right (if (>= (- (- highest-x x1) 1) (- ship-length 1)) (- ship-length 1) (- (- highest-x x1) 1)))
-  (define free-blocks-left (if (>= (- (- x1 least-x) 1) (- ship-length 1)) (- ship-length 1) (- (- x1 least-x) 1)))
-  (define free-blocks-down (if (>= (- (- highest-y y1) 1) (- ship-length 1)) (- ship-length 1) (- (- highest-y y1) 1)))
-  (define free-blocks-up (if (>= (- (- y1 least-y) 1) (- ship-length 1)) (- ship-length 1) (- (- y1 least-y) 1)))
-  
-  (define no-of-ways 0)
-  (cond [(and (= free-blocks-right (- ship-length 1)) (= free-blocks-left (- ship-length 1))) (set! no-of-ways (+ no-of-ways ship-length))]
-        [(= free-blocks-right (- ship-length 1)) (set! no-of-ways (+ no-of-ways free-blocks-left 1))]
-        [(= free-blocks-left (- ship-length 1)) (set! no-of-ways (+ no-of-ways free-blocks-right 1))]
-        [(>= (+ free-blocks-right free-blocks-left 1) ship-length) (set! no-of-ways (+ no-of-ways (- (+ free-blocks-right free-blocks-left 1) ship-length) 1))])
-  (cond [(and (= free-blocks-down (- ship-length 1)) (= free-blocks-up (- ship-length 1))) (set! no-of-ways (+ no-of-ways ship-length))]
-        [(= free-blocks-down (- ship-length 1)) (set! no-of-ways (+ no-of-ways free-blocks-up 1))]
-        [(= free-blocks-up (- ship-length 1)) (set! no-of-ways (+ no-of-ways free-blocks-down 1))]
-        [(>= (+ free-blocks-down free-blocks-up 1) ship-length) (set! no-of-ways (+ no-of-ways (- (+ free-blocks-down free-blocks-up 1) ship-length) 1))])
-  no-of-ways)
 
+(define-syntax lc
+  (syntax-rules (: <- @)
+    [(lc expr : var <- drawn-from) (map (lambda (var) expr) drawn-from)]
+    [(lc expr : @ guard) (if guard (list expr) `())]
+    [(lc expr : @ guard  qualifier ...) 
+     (append* (lc (lc expr : qualifier ...) : @ guard))]
+    [(lc expr : var <- drawn-from  qualifier ...) 
+     (append* (lc (lc expr :  qualifier ... ) : var <- drawn-from))]))
+
+;returns pair of two lists, first one has coordinates of points having 1/3 and second has coords with 2. to be called with 0 0 '() '()
+;;OPTIMISATION: should be updated after move of computer, not calculated every time
+(define (forbidden-and-hit-points strikes-grid r c forbid-points hit-points)
+  (cond[(and (= r 9) (= c 9)) (cond[(or (= (grid-ref strikes-grid r c) 1) (= (grid-ref strikes-grid r c) 3)) (cons (cons (cons r c) forbid-points) hit-points)]
+                                   [(= (grid-ref strikes-grid r c) 2) (cons forbid-points (cons (cons r c) hit-points))]
+                                   [else (cons forbid-points hit-points)])]
+       [(equal? c 9) (cond[(or (= (grid-ref strikes-grid r c) 3) (= (grid-ref strikes-grid r c) 1)) (forbidden-and-hit-points strikes-grid (+ r 1) 0 (cons (cons r c) forbid-points) hit-points)]
+                          [(= (grid-ref strikes-grid r c) 2) (forbidden-and-hit-points strikes-grid (+ r 1) 0 forbid-points (cons (cons r c) hit-points))]
+                          [else (forbidden-and-hit-points strikes-grid (+ r 1) 0 forbid-points hit-points)])]
+       [else (cond[(or (= (grid-ref strikes-grid r c) 1) (= (grid-ref strikes-grid r c) 3)) (forbidden-and-hit-points strikes-grid r (+ c 1) (cons (cons r c) forbid-points) hit-points)]
+                  [(= (grid-ref strikes-grid r c) 2) (forbidden-and-hit-points strikes-grid r (+ c 1) forbid-points (cons (cons r c) hit-points))]
+                  [else (forbidden-and-hit-points strikes-grid r (+ c 1) forbid-points hit-points)])]))
+
+
+;calculates the number of ways to place a ship given free-blocks-pos and neg as the number of blocks between the current block and the nearest forbidden one in the pos and neg direction
+;ship-length>=0
+(define (calc_ways free-blocks-pos free-blocks-neg ship-length)
+  (begin (set! free-blocks-pos (min free-blocks-pos (- ship-length 1)))
+         (set! free-blocks-neg (min free-blocks-neg (- ship-length 1)))
+         (cond [(and (= free-blocks-pos (- ship-length 1)) (= free-blocks-neg (- ship-length 1))) ship-length]
+               [(= free-blocks-pos (- ship-length 1)) (+ free-blocks-neg 1)]
+               [(= free-blocks-neg (- ship-length 1)) (+ free-blocks-pos 1)]
+               [(>= (+ free-blocks-pos free-blocks-neg 1) ship-length) (+ (- (+ free-blocks-pos free-blocks-neg 1) ship-length) 1)]
+               [else 0])))
+
+;returns the possible ways a part of a ship could be at x1,y1, provided the list of forbidden points and the length of the ship
+;forbid-pt-list is list of points with status 1 or 3, through-pt-list is list of points with status 2
+(define (ways-for-sqr x1 y1 forbid-pt-list through-pt-list ship-length)
+  (let ([forbid-x-right 10]
+        [forbid-y-down 10]
+        [forbid-x-left -1]
+        [forbid-y-up -1]
+        ;check that they may remian these values till the end
+        [through-x-right 10]
+        [through-y-down 10]
+        [through-x-left -1]
+        [through-y-up -1]
+        [no-of-ways 0])
+    (cond [(list? (member (cons y1 x1) forbid-pt-list)) 0];test if the given point itslef is forbidden
+          [(null? through-pt-list) 
+           (begin 
+             ;determine nearest points in all four directions which are forbidden
+             (map (lambda (coord) (cond [(= (cdr coord) x1) (cond [(and (> (car coord) y1) (< (car coord) forbid-y-down)) (set! forbid-y-down (car coord))]
+                                                                  [(and (< (car coord) y1) (> (car coord) forbid-y-up)) (set! forbid-y-up (car coord))])]
+                                        [(= (car coord) y1) (cond [(and (> (cdr coord) x1) (< (cdr coord) forbid-x-right)) (set! forbid-x-right (cdr coord))]
+                                                                  [(and (< (cdr coord) x1) (> (cdr coord) forbid-x-left)) (set! forbid-x-left (cdr coord))])]))
+                  forbid-pt-list)
+             (set! no-of-ways (+ no-of-ways (calc_ways (- (- forbid-x-right x1) 1) (- (- x1 forbid-x-left) 1) ship-length)))
+             (set! no-of-ways (+ no-of-ways (calc_ways (- (- forbid-y-down y1) 1) (- (- y1 forbid-y-up) 1) ship-length)))
+             ;calculate the number of blocks where a part of ship could be placed in each direction, keeping in mind that x1,y1 should always be occupied
+             no-of-ways)]
+          [else
+           (begin
+             ;determine nearest points in all four directions which are forbidden
+             (map (lambda (coord) (cond [(= (cdr coord) x1) (cond [(and (> (car coord) y1) (< (car coord) forbid-y-down)) (set! forbid-y-down (car coord))]
+                                                                  [(and (< (car coord) y1) (> (car coord) forbid-y-up)) (set! forbid-y-up (car coord))])]
+                                        [(= (car coord) y1) (cond [(and (> (cdr coord) x1) (< (cdr coord) forbid-x-right)) (set! forbid-x-right (cdr coord))]
+                                                                  [(and (< (cdr coord) x1) (> (cdr coord) forbid-x-left)) (set! forbid-x-left (cdr coord))])]))
+                  forbid-pt-list)
+             ;determine nearest points in all four directions which are compulsory
+             (map (lambda (coord) (cond [(= (cdr coord) x1) (cond [(and (> (car coord) y1) (< (car coord) through-y-down)) (set! through-y-down (car coord))]
+                                                                  [(and (< (car coord) y1) (> (car coord) through-y-up)) (set! through-y-up (car coord))])]
+                                        [(= (car coord) y1) (cond [(and (> (cdr coord) x1) (< (cdr coord) through-x-right)) (set! through-x-right (cdr coord))]
+                                                                  [(and (< (cdr coord) x1) (> (cdr coord) through-x-left)) (set! through-x-left (cdr coord))])]))
+                  through-pt-list)
+             (cond [(and (<= forbid-x-right through-x-right) (>= forbid-x-left through-x-left)) 0]
+                   [(>= forbid-x-left through-x-left) (if (< (- through-x-right x1) ship-length)
+                                                          (set! no-of-ways (+ no-of-ways (calc_ways (- (- forbid-x-right through-x-right) 1)
+                                                                                                    (- (- x1 forbid-x-left) 1) (- ship-length (- through-x-right x1)))))
+                                                          (void))]
+                   [(<= forbid-x-right through-x-right) (if (< (- x1 through-x-left) ship-length)
+                                                            (set! no-of-ways (+ no-of-ways (calc_ways (- (- forbid-x-right x1) 1)
+                                                                                                      (- (- through-x-left forbid-x-left) 1) (- ship-length (- x1 through-x-left)))))
+                                                            (void))]
+                   [else (begin (if (< (- x1 through-x-left) ship-length)
+                                    (set! no-of-ways (+ no-of-ways (calc_ways (- (- through-x-right x1) 1)
+                                                                              (- (- through-x-left forbid-x-left) 1) (- ship-length (- x1 through-x-left)))))
+                                    (void))
+                                (if (< (- through-x-right x1) ship-length)
+                                    (set! no-of-ways (+ no-of-ways (calc_ways (- (- forbid-x-right through-x-right) 1)
+                                                                              (- (- x1 through-x-left) 1) (- ship-length (- through-x-right x1)))))
+                                    (void))
+                                (if (< (- through-x-right through-x-left) ship-length)
+                                    (set! no-of-ways (+ no-of-ways (calc_ways (- (- forbid-x-right through-x-right) 1)
+                                                                              (- (- through-x-left forbid-x-left) 1) (- ship-length (- through-x-right through-x-left)))))
+                                    (void)))])
+             no-of-ways)])))
+(define (numbers-grid strikes-grid rem-lengths)
+  (define grid (build-grid 10 10 0))
+  (define pair (forbidden-and-hit-points strikes-grid 0 0 '() '()))
+  (lc (set-grid! grid r c (ways-for-sqr c r (car pair) (cdr pair) length)) : r <- (list 0 1 2 3 4 5 6 7 8 9) c <- (list 0 1 2 3 4 5 6 7 8 9) length <- rem-lengths)
+  grid)
 
     
 
@@ -86,7 +158,7 @@
     (if (makes-sense? line) line (random-ship length))))
  
 (define (random-line length)
-  (define start (cons (random 1 11) (random 1 11)))
+  (define start (cons (random 0 10) (random 0 10)))
   (define dirn (list-ref '("up" "down" "left" "right") (random 0 4)))
   (extend-line start dirn length))
 
@@ -109,34 +181,32 @@
 
 (define (makes-sense? line)   ;  makes sure no coordinate is out of the grid
   (define (inside? coord)
-    (if (and (and (> (car coord) 0) (< (car coord) 11))
-             (and (> (cdr coord) 0) (< (cdr coord) 11)))
+    (if (and (and (> (car coord) -1) (< (car coord) 10))
+             (and (> (cdr coord) -1) (< (cdr coord) 10)))
         #t
         #f))
   (andmap inside? line))
 
-  
+ (define (random-ships)
+   (define l '(2 3 3 4 5))
+   (ships-helper l '()))
+
+(define (ships-helper l c)
+  (cond[(null? l) (append* (reverse c))]
+       [else (let ([res (random-ship (car l))])
+               (begin (displayln res)
+               (cond[(satisfy? res c) (ships-helper (cdr l) (cons res c))]
+                    [else (ships-helper l c)])))]))
+
+(define (satisfy? res c)
+  (let* ([res-list  (lc (member x y) : x <- res y <- c )]  ;(map (lambda (x) (member res x)) c)]
+         [all (filter (lambda (x) (not (equal? x #f))) res-list)])
+    (displayln res-list)
+    (cond[(= (length all) 0) #t]
+         [else #f])))
+    
 
 
 
 
-
-
-
-
-
-
-
-;;special function that saumya asked to form. returns two lists . First one has coordinates of points having 1/3 and second has coords with 2.
-
-(define (spec-func strikes-grid r c l1 l2)
-  (cond[(and (= r 9) (= c 9)) (cond[(or (= (grid-ref strikes-grid r c) 1) (= (grid-ref strikes-grid r c) 3)) (cons (cons (cons r c) l1) l2)]
-                                   [(= (grid-ref strikes-grid r c) 2) (cons l1 (cons (cons r c) l2))]
-                                   [else (cons l1 l2)])]
-       [(equal? c 9) (cond[(or (= (grid-ref strikes-grid r c) 3) (= (grid-ref strikes-grid r c) 1)) (spec-func strikes-grid (+ r 1) 0 (cons (cons r c) l1) l2)]
-                          [(= (grid-ref strikes-grid r c) 2) (spec-func strikes-grid (+ r 1) 0 l1 (cons (cons r c) l2))]
-                          [else (spec-func strikes-grid (+ r 1) 0 l1 l2)])]
-       [else (cond[(or (= (grid-ref strikes-grid r c) 1) (= (grid-ref strikes-grid r c) 3)) (spec-func strikes-grid r (+ c 1) (cons (cons r c) l1) l2)]
-                  [(= (grid-ref strikes-grid r c) 2) (spec-func strikes-grid r (+ c 1) l1 (cons (cons r c) l2))]
-                  [else (spec-func strikes-grid r (+ c 1) l1 l2)])]))
 
